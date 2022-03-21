7ae09f9b1a906fe413b07fae43a6b0c1f084d523
nathan@river <nathanmcninch@gmail.com>
2022-03-21 05:44:04 -0700

    add lab starter code

---
+++ examples/app.js
@@ -0,0 +1,5 @@
+// const divone = document.querySelector('.one')
+// divone.classList.add('pink-background')
+
+// const divtwo = document.querySelector('.two')
+// divtwo.classList.toggle('blue-background')
