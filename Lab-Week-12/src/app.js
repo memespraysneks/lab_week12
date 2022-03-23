ecd8d401b34aaa032f3301b2fc28b9fc735587df
alsomemespraysnek <84824178+alsomemespraysnek@users.noreply.github.com>
2022-03-23 00:49:03 -0700

    Made JS to change the theme from light to dark

--- Lab-Week-12/src/app.js
+++ Lab-Week-12/src/app.js
@@ -1,2 +1,10 @@
 let selection = document.querySelector(".one")
 console.log(selection)
+
+function darkmode () {
+    let change = document.querySelector(".light-theme")
+    change.classList.remove("light-theme")
+    change.classList.add("dark-theme")
+}
+
+darkmode()
